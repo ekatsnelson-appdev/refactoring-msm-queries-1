536590eda89acdcc5939cd24431ab66ac7e4ebc7
ekatsnelson <elisek222@gmail.com>
2022-11-01 22:47:45 +0000

    started movie refactoring

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -13,4 +13,12 @@
 #  director_id :integer
 #
 class Movie < ApplicationRecord
+  def director
+    # Get the foreign key, director_id
+    # Query the Director table in the ID column with it
+    # Get the single match and return it
+    matching_directors = Director.where({ :id => self.director_id })
+    the_director = matching_directors.at(0)
+    return the_director
+  end
 end
